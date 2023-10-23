016f19142e0dd475d5c3afe73637bd1a0222834e
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-22 23:32:46 -0700

    Initial Commit

---
+++ inspiration-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  inspiration-app
+//
+//  Created by jackson mowatt gok on 22/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ inspiration-app/inspiration_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  inspiration_appApp.swift
+//  inspiration-app
+//
+//  Created by jackson mowatt gok on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct inspiration_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
